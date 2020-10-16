75029416e674c293edef9f35f56ed1ae478be4a1
Akash <akashpatil1023@gmail.com>
2020-10-16 18:13:03 -0400

    Codes

---
+++ create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/Users/akash/Videos/DL_SSD/VOCdevkit/VOC2007',
+                      voc12_path='/Users/akash/Videos/DL_SSD/VOCdevkit/VOC2012',
+                      output_folder='./')
